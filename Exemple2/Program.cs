28a4ea16ffe498a4bb9bfd189282c29bec3eae81
MaksTimoshenko <124436224+MakcTiMoshenko@users.noreply.github.com>
2023-02-26 13:06:49 +0300

    Задача 36
    Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
    
    [3, 7, 23, 12] -> 19
    
    [-4, -6, 89, 6] -> 0

---
+++ Exemple2/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами.
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] GenArray(int size, int leftRange, int rightRange)
+{
+    int[] array = new int[size];
+    Random random = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = random.Next(leftRange, rightRange + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.WriteLine("[" + string.Join(", ", array) + "]");
+}
+int CountElements(int[] array, int leftRange, int rightRange)
+{
+    int sum = 0;
+    for (int i = 1; i < array.Length; i=i+2)
+    {
+        sum = sum + array[i];
+        
+    }
+    return sum;
+}
+
+var myArray = GenArray(8, -99, 100);
+PrintArray(myArray);
+Console.WriteLine(CountElements(myArray, 1, 100));
